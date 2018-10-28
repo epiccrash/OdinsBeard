aca8907fbd56ccd5ccfddeebe6f63b3738dcc7cd
Charlie Liu <jl6@andrew.cmu.edu>
2018-10-28 12:29:05 -0400

    ..

---
+++ Assets/Scripts/ProjectileScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectileScript : MonoBehaviour {
+
+    public Rigidbody projectile;
+    public float projectileSpeed = 14.0f;
+
+	// Use this for initialization
+	void Start () 
+    {
+        
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        if (Input.GetKeyDown("A"))
+        {
+
+        }
+    }
+}
